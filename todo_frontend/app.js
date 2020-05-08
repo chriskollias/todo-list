6de1b0f17cb5ee251665f9b8abf3e878f23fca6d
chriskollias <chriskollias500@gmail.com>
2020-05-08 11:36:45 -0700

    frontend now loading existing ToDos from database

--- todo_frontend/app.js
+++ todo_frontend/app.js
@@ -11,6 +11,9 @@ filterOption.addEventListener('click', filterTodos);
 //this one is for when the document first loads
 document.addEventListener('DOMContentLoaded', loadTodos);
 
+//Backend API endpoint
+const url = 'http://127.0.0.1:8000/';
+
 //Functions
 function addTodo(event) {
     //prevent form from submitting (which means refreshing on click)
@@ -119,12 +122,65 @@ function filterTodos(event) {
 
 //save a todo to the backend database
 function saveTodo(){
-    
+
+
 }
 
 //load todos from the backend database
 function loadTodos(){
-    
+    const Http = new XMLHttpRequest();
+    Http.open('GET', url);
+
+
+    // callback for when we receive response
+    Http.onload = () => {
+        console.log(Http.responseText);
+        responseData = JSON.parse(Http.responseText);
+        console.log('Respone data in JSON:');
+        console.log(responseData[0]);
+
+        // add each todo in the response to our list
+        responseData.forEach(todo => {
+
+            const todoDiv = document.createElement('div');
+            todoDiv.classList.add('todo');
+
+            //Create <li> that will be placed inside of the new div
+            const todoLi = document.createElement('li');
+            todoLi.classList.add('todo-item');
+
+            //place a value inside of the new <li>
+            todoLi.innerText = todo['title'];
+
+            //actually place the <li> inside of the new div
+            todoDiv.appendChild(todoLi);
+
+            //create checkmark button
+            const completedButton = document.createElement('button');
+
+            //insert HTML inside of the button element we just created, as opposed to text
+            completedButton.innerHTML = '<i class="fas fa-check"></i>';
+
+            completedButton.classList.add("complete-btn");
+            todoDiv.appendChild(completedButton);
+
+            //create trash button
+            const trashButton = document.createElement('button');
+            trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+            trashButton.classList.add("trash-btn");
+            todoDiv.appendChild(trashButton);
+
+            //finally append the new div to the todo list
+            todoList.appendChild(todoDiv);
+
+            //check to see if it's completed, if so show it as such
+            if(todo["completed"]){
+                todoDiv.classList.add('completed');
+            }
+        })
+    }
+
+    Http.send();
 }
 
 //delete a todo from the backend database
